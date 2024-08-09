3776a169776c9c1282ae008c06fd511674edfa71
Shahir Ahmed <ahmeds@dickinson.edu>
2024-08-09 16:48:48 -0400

    chore: Update HTML and CSS for Soccer Memory Game

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import "../styles/Card.css";
 function Card({ image, title, onClick }) {
 	return (
 		<div className="card" onClick={onClick}>
-			<img src={image} alt={title} />
+			<img src={image} alt={title} loading="lazy" />
 			<h2>{title}</h2>
 		</div>
 	);
